429cd6b22812baeb9f6a82c5d20fa1e238416d93
Bertrand Marron <bertrand.marron@gmail.com>
2020-05-12 18:25:46 +0200

    Update getInitialProps in Next.js SSR example
    Stop calling renderPage directly as it may prevent enhancers from
    executing.
    Create style element from getInitialProps directly.

--- examples/ssr-next/pages/_document.js
+++ examples/ssr-next/pages/_document.js
@@ -1,11 +1,10 @@
-/* eslint-disable react/no-danger */
 import React from 'react'
 import Document, { Head, Main, NextScript } from 'next/document'
 import { extractStyles } from 'evergreen-ui'
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const page = renderPage()
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
     // `css` is a string with css from both glamor and ui-box.
     // No need to get the glamor css manually if you are using it elsewhere in your app.
     //
@@ -16,20 +15,19 @@ export default class MyDocument extends Document {
     const { css, hydrationScript } = extractStyles()
 
     return {
-      ...page,
-      css,
+      ...initialProps,
+      styles: [...initialProps.styles, React.createElement('style', null, css)],
       hydrationScript
     }
   }
 
   render() {
-    const { css, hydrationScript } = this.props
+    const { hydrationScript } = this.props
 
     return (
       <html>
         <Head>
           <title>SSR in Next.js</title>
-          <style dangerouslySetInnerHTML={{ __html: css }} />
         </Head>
 
         <body>
